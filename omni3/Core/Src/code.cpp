664bfc018a542c2927f8234e6a40b5ff025cc13e
sosukigara <s25089@tokyo.kosen-ac.jp>
2025-12-17 20:42:13 +0900

    feat : add imu

--- omni3/Core/Src/code.cpp
+++ omni3/Core/Src/code.cpp
@@ -1,22 +1,22 @@
 // Core includes
 #include <main.h>
+
 #include <code.hpp>
+
 
 // Project headers
 #include <cstdint>
+#include <optional>
 #include <param.hpp>
 #include <shared.hpp>
 #include <tr/prelude.hpp>
-#include <optional>
 
-// Local helper: keep HeadState and read function inside this translation unit
-// to avoid cross-TU symbol duplication and make code self-contained.
 struct HeadState {
     Qty<Radian> angle = 0_rad;
     Qty<RadianPerSecond> angvel = 0_radps;
 };
 
-static EnumMap<mechs::omni3::Id, HeadState> read_head_states_local(mods::Dji<Fdcan>* dji) {
+static EnumMap<mechs::omni3::Id, HeadState> read_head_states_local(mods::Dji<Fdcan> *dji) {
     EnumMap<mechs::omni3::Id, HeadState> res;
     for (const mechs::omni3::Id id : AllVariants<mechs::omni3::Id>()) {
         const mods::dji::Id djid = OMNI3_TO_DJI[id];
@@ -26,32 +26,27 @@ static EnumMap<mechs::omni3::Id, HeadState> read_head_states_local(mods::Dji<Fdc
     return res;
 }
 
-
-
 extern FDCAN_HandleTypeDef hfdcan1;
 extern FDCAN_HandleTypeDef hfdcan2;
 extern FDCAN_HandleTypeDef hfdcan3;
 extern TIM_HandleTypeDef htim6;
 extern TIM_HandleTypeDef htim7;
-extern I2C_HandleTypeDef hi2c3; 
+extern I2C_HandleTypeDef hi2c3;
 
 bool initialized = false;
 
-mods::Terunet3 *tn3; 
+mods::Terunet3 *tn3;
 
 Qty<Radian> now_angle = 0_rad;
 Qty<RadianPerSecond> now_angvel = 0_radps;
 Qty<Radian> target_angle = 0_rad;
 
+// imu
 
-//imu
-
-mods::Bno055* imu;
+mods::Bno055 *imu;
 Qty<Radian> yaw;
 
-
-mods::Dji<Fdcan> *dji; 
-
+mods::Dji<Fdcan> *dji;
 
 EnumMap<mechs::omni3::Id, HeadState> now_states_global = {};
 
@@ -59,29 +54,25 @@ static inline float head_angle_deg(const HeadState &s) {
     return s.angle.get_value() * 180.0F / 3.141592653589793F;
 }
 
-static inline Qty<Radian> normalize_angle(const Qty<Radian> a) {
-    
-    return a;
-}
+static inline Qty<Radian> normalize_angle(const Qty<Radian> a) { return a; }
 
-//omni3
+// omni3
 mechs::omni3::Ik ik = mechs::omni3::Ik(OMNI3_CONFIG);
 Transform2d target_transform = {0_mps, 0_mps, 0_radps};
 
-Transform2d target_transform_max = {3_mps, 3_mps, 0_radps}; 
+Transform2d target_transform_max = {3_mps, 3_mps, 0_radps};
 
-controllers::ControlPosition<Qty<Radian>, Qty<Ampere>>* cp;
+controllers::ControlPosition<Qty<Radian>, Qty<Ampere>> *cp;
 
-EnumMap<mechs::omni3::Id, std::optional<ctls::ControlVelocity<Qty<Radian>, Qty<Ampere>>>> cvs; 
+EnumMap<mechs::omni3::Id, std::optional<ctls::ControlVelocity<Qty<Radian>, Qty<Ampere>>>> cvs;
 
 void get_terunet() {}
 void set_terunet() {}
 
-
 void setup() {
     disable_irq();
 
-//imu
+    // imu
     imu = new mods::Bno055(new hardwares::I2cMaster(&hi2c3));
 
     tn3 = new mods::Terunet3(new Fdcan(&hfdcan1));
@@ -91,7 +82,6 @@ void setup() {
 
     dji = new mods::Dji<Fdcan>(new Fdcan(&hfdcan3), DJI_SAMPLING_PERIOD, DJI_DETAILS);
 
-  
     for (const mechs::omni3::Id id : AllVariants<mechs::omni3::Id>()) {
         cvs[id].emplace(TIMER_PERIOD, CV_PARAM, CV_CONFIG, true);
     }
@@ -99,37 +89,31 @@ void setup() {
     initialized = true;
 
     enable_irq();
-} 
+}
 
 void loop() {
     get_terunet();
 
-    //IMU
-    imu ->rx();
-    imu ->update();
-    yaw = imu -> get_yaw();
+    // IMU
+    imu->rx();
+    imu->update();
+    yaw = imu->get_yaw();
 
-    //omni3
-    ik.set_heading(yaw); //imu
+    // omni3
+    ik.set_heading(yaw);  // imu
     ik.set_transform(target_transform);
     ik.update();
 
-
     now_states_global = read_head_states_local(dji);
-
-
-
-
-
 
     for (const mechs::omni3::Id id : AllVariants<mechs::omni3::Id>()) {
         cvs[id]->set_target_velocity(ik.get_velocity(id) / WHEEL_RADIUS);
         cvs[id]->set_now_velocity(dji->get_now_head_angvel(OMNI3_TO_DJI[id]).unwrap());
         dji->set_target_current(OMNI3_TO_DJI[id], cvs[id]->get_output());
     }
-    
+
     set_terunet();
-} 
+}
 
 #ifdef HAL_TIM_MODULE_ENABLED
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
@@ -153,7 +137,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
         return;
     }
 }
-#endif 
+#endif
 
 #ifdef HAL_FDCAN_MODULE_ENABLED
 void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t rx_fifo0_its) {
